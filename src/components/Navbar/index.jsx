496918408f0bb4ea5dbf369e8ca3678e22621021
Dhanidayo <sarahjoseph@ymail.com>
2022-03-05 20:19:23 +0100

    working on the landing page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,33 +1,34 @@
 import { Link, NavLink } from 'react-router-dom';
+import "./style.css";
 
 const Navbar = () => {
     return (
         <header>
-            <div className="container--primary">
-                <Link to="/">
-                    LOGO-HERE
+            <div className="container--primary row">
+                <Link className='logo nav__link' to="/">
+                    GIRLS-IN-TECH
                 </Link>
                 <nav className="navbar">
                     <ul className="nav__list nav__list--primary">
                         <li className="nav__item">
-                            <NavLink to="/blog">Blog</NavLink>
+                            <NavLink className="nav__link" to="/blog">Blog</NavLink>
                         </li>
                         <li className="nav__item">
-                            <NavLink to="/about">About Us</NavLink>
+                            <NavLink className="nav__link" to="/about">About Us</NavLink>
                         </li>
                         <li className="nav__item">
-                            <NavLink to="/login">Contact Us</NavLink>
+                            <NavLink className="nav__link" to="/login">Contact Us</NavLink>
                         </li>
                         <li className="nav__item">
-                            <NavLink to="/">Donate</NavLink>
+                            <NavLink className="nav__link" to="/">Donate</NavLink>
                         </li>
                     </ul>
                     <ul className="nav__list nav__list--secondary">
                         <li className="nav__item">
-                            <NavLink to="/">English</NavLink>
+                            <NavLink className="nav__link" to="/">English</NavLink>
                         </li>
                         <Link to="/sign-up">
-                            <button className="btn--primary">
+                            <button className="btn btn--primary">
                                 Get Started
                             </button>
                         </Link>
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,30 +1,108 @@
 import DefaultLayout from "../../components/Layout";
 import { Link } from "react-router-dom";
+import bannerimg from "../../resources/images/Hero Image.png";
+import mentorsillustration from "../../resources/illustration-images/Group 185.png";
+import noteillustration from "../../resources/illustration-images/Group 184.png";
+import briefcaseillustration from "../../resources/illustration-images/Group 211.png";
+import "./style.css";
 
 const LandingPage = () => {
     return (
         <DefaultLayout>
             <div className="landing-page">
-                <div className="container--primary">
-                    <div className="banner-contents-wrapper">
-                        <div className="banner-texts">
-                            <h1 className="main-text">
-                                Get Access to Life-changing Opportunities
-                            </h1>
-                            <p className="main-para">
-                                Become a part of our community and get unlimited access to resources, mentors, and connect with other women like you who are changing the world.
-                            </p>
-                            <Link to="/">
-                                <button className="btn--primary">
-                                    Get Started
-                                </button>
-                            </Link>
-                        </div>
-                        <div className="banner-image">
-
+                <section id="banner">
+                    <div className="container--primary">
+                        <div className="banner-contents-wrapper row">
+                            <div className="banner-texts">
+                                <h1 className="main-text">
+                                    Get Access to<br />
+                                    Life-changing<br /> Opportunities
+                                </h1>
+                                <p className="main-para body-text">
+                                    Become a part of our community and get unlimited <br/> access to resources, mentors, and connect with <br /> other women like you who are changing the world.
+                                </p>
+                                <Link to="/">
+                                    <button className=" btn btn--primary">
+                                        Get Started
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="banner-image">
+                                <img className="hero-image" src={bannerimg }alt="" />
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
+
+                <section id="benefits-section">
+                    <div className="container--primary">
+                        <h2 className="sub-heading">
+                            What our members benefit
+                        </h2>
+                        <div className="cards-container">
+                            <div className="flex-cards">
+                                <div className="card">
+                                    <img className="illustration-image" src={mentorsillustration} alt=""  />
+                                    <p className="card-title">
+                                        Connect to career mentors
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <img className="illustration-image"  src={mentorsillustration} alt=""  />
+                                    <p className="card-title">
+                                        Working with other techsters
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex-cards">
+                                <div className="card">
+                                    <img className="illustration-image"  src={noteillustration} alt=""  />
+                                    <p className="card-title">
+                                        Unlimited resources
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <img className="illustration-image"  src={noteillustration} alt=""  />
+                                    <p className="card-title">
+                                        Weekly trainings and projects
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex-cards">
+                                <div className="card">
+                                    <img className="illustration-image"  src={briefcaseillustration} alt=""  />
+                                    <p className="card-title">
+                                        Job Opportunities and Career Roadmaps
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                                <div className="card">
+                                    <img className="illustration-image"  src={briefcaseillustration} alt=""  />
+                                    <p className="card-title">
+                                        Scholarship and Internships
+                                    </p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, tempora officiis mollitia ullam fugiat dolores.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </DefaultLayout>
     );
